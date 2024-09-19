dd22e53c062e9ea3fda344fe18d0e595502359e1
rubenavaldez <rubenavaldez@yahoo.com>
2024-09-18 20:34:29 -0700

    added card map with dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,35 @@
-import Welcome from '@/components/welcome';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function Home() {
+  const cards = [
+    { title: 'title', description: 'description', content: 'content', footer: 'footer' },
+    { title: 'title 2', description: 'description 2', content: 'content 2', footer: 'footer 2' },
+  ]; // test data
   return (
     <main className="flex flex-col gap-10 p-10">
-      <Welcome />
+      {cards.map((card, i) => {
+        return (
+          <Card key={'card+' + i}>
+            <CardHeader>
+              <CardTitle>{card.title}</CardTitle>
+              <CardDescription>{card.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{card.content}</p>
+            </CardContent>
+            <CardFooter>
+              <p>{card.footer}</p>
+            </CardFooter>
+          </Card>
+        );
+      })}
     </main>
   );
 }
